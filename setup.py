0153ebd02b2fa3247f2e19489b28b78309b06555
Zdenek Svoboda <zsvoboda@gmail.com>
2022-01-08 01:01:27 +0100

    0.6.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ REQUIRES = ["jinja2", "sqlalchemy", "sqlparse", "sql-metadata", "networkx", "psy
 setup(
     name="dbd",
     description="Declarative database",
-    version="0.6.1",
+    version="0.6.2",
     author="Zdenek Svoboda",
     license="BSD",
     install_requires=REQUIRES,
